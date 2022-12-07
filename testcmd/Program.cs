4a8a5daadd81d139f9cd5af5916617f97d9cc4db
Kunlapath <kunlapath.paengsa@gmail.com>
2022-12-07 12:39:57 +0700

    Add project files.

---
+++ testcmd/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Diagnostics;
+
+namespace testcmd
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("node -n");
+            Process process = new Process();
+            process.StartInfo.FileName = "cmd.exe";
+            process.StartInfo.CreateNoWindow = true;
+            process.StartInfo.RedirectStandardInput = true;
+            process.StartInfo.RedirectStandardOutput = true;
+            process.StartInfo.UseShellExecute = false;
+            process.Start();
+            process.StandardInput.WriteLine("node -v");
+            process.StandardInput.Flush();
+            process.StandardInput.Close();
+            process.WaitForExit();
+            Console.WriteLine(process.StandardOutput.ReadToEnd());
+            Console.ReadLine();
+        }
+    }
+}
